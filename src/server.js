4d036ade73a40aa5dc6c151d1e83bc067f771345
Natan <zantanad.24@gmail.com>
2023-06-22 05:49:29 +0700

    postgres database implemented

--- src/server.js
+++ src/server.js
@@ -1,13 +1,14 @@
+require('dotenv').config();
 const Hapi = require('@hapi/hapi');
 const notes = require('./api/notes');
-const NotesService = require('./service/inMemory/NoteService');
+const NotesService = require('./service/postgres/NotesService');
 const NotesValidator = require('./validator/notes');
 
 const init = async () => {
   const notesService = new NotesService();
   const server = Hapi.server({
-    port: 5000,
-    host: process.env.NODE_ENV !== 'production' ? 'localhost' : '0.0.0.0',
+    port: process.env.PORT,
+    host: process.env.HOST,
     routes: {
       cors: {
         origin: ['*'],
